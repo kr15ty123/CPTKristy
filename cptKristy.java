9e8a476566081e3a2841fbd19cd032e5c5d8ae76
kr15ty123 <kristy.lor26@ycdsbk12.ca>
2025-06-09 09:36:36 -0400

    added themes files

--- cptKristy.java
+++ cptKristy.java
@@ -4,6 +4,10 @@ public class cptKristy{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Helloooooo");
+		//opening files
+		TextInputFile themes = new TextInputFile("themes.txt");	
+		
+
 		
 	}
 }
